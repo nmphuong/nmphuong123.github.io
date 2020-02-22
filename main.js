30b9b79813f9137e63b59356df7771b2e1eefc4b
nmphuong <56857408+nmphuong@users.noreply.github.com>
2020-02-22 20:13:11 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+const socket = io('https://webrtcv1demo.herokuapp.com/');
+
+$('#divChat').hide();
+socket.on('List_Online', arrUserInfo => {
+
+        $('#divChat').show();
+        $('#divRegister').hide();
+        arrUserInfo.forEach(user => {
+                const{ten, peerId} = user;
+                $('#ulUser').append(`<button type="button" id="${peerId}">${ten}</button>`);
+        });
+        socket.on('have_a_register', user => {
+                const{ten, peerId} = user;
+                $('#ulUser').append(`<button type="button" id="${peerId}">${ten}</button>`);
+        });
+        socket.on('user_disconnect', peerId => {
+                $(`#${peerId}`).remove();
+        });
+});
+
+socket.on('register_fail', () => alert('Please choose another username!'));
+
+function openStream(){
+        const config = {audio: true, video: true};
+        return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream){
+        const video = document.getElementById(idVideoTag);
+        video.srcObject = stream;
+        video.play();
+}
+// openStream()
+// .then(stream => playStream('localStream', stream));
+
+const peer = new Peer({key: 'lwjd5qra8257b9'});
+
+peer.on('open', id => {
+        $('#my-peer').append(id)
+        $('#btnSignUp').click(() => {
+                const username = $('#txtUsername').val();
+                socket.emit('User_Register',{ ten: username, peerId: id});
+        });
+});
+
+//Local
+$('#btnCall').click(() => {
+        const id = $('#remoteId').val();
+        openStream()
+        .then(stream =>  {
+                playStream('localStream', stream);
+                const call = peer.call(id, stream);
+                call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+        });
+});
+
+//Remote
+peer.on('call', call => {
+        openStream()
+        .then(stream => {
+                call.answer(stream);
+                playStream('localStream', stream);
+                call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+        })
+})
+
+$('#ulUser').on('click', 'button', function(){
+        const id = $(this).attr('id');
+        openStream()
+        .then(stream =>  {
+                playStream('localStream', stream);
+                const call = peer.call(id, stream);
+                call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+        });
+});
